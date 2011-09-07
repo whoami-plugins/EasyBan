972f61037efee6a2a72b530f0030e81c800baf7d
Sebastian Köhler <sebkoehler@whoami.org.uk>
2011-09-07 12:58:44 +0200

    license

--- src/uk/org/whoami/easyban/commands/EasyBanCommand.java
+++ src/uk/org/whoami/easyban/commands/EasyBanCommand.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2011 Sebastian Köhler <sebkoehler@whoami.org.uk>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package uk.org.whoami.easyban.commands;
 
 import org.bukkit.command.Command;
@@ -6,7 +21,6 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import uk.org.whoami.easyban.settings.Message;
 import uk.org.whoami.easyban.permission.Permission;
-
 
 public abstract class EasyBanCommand implements CommandExecutor {
     
